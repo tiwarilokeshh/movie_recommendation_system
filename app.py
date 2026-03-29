63105d634b5035c703ac229feb44127da049a4a3
Lokesh Tiwari <tiwarilokesh796@gmail.com>
2026-03-30 01:01:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=f6d6951a66e9c732ca244148db2ca483&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for v in movie_list:
+        movie_id = movies.iloc[v[0]].movie_id
+
+        recommended_movies.append(movies.iloc[v[0]].title)
+        # fetch poster from api
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies = pd.DataFrame(movies_list)
+
+movies_list = movies_list['title'].values
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie Recommendation System')
+
+selected_movie_name = st.selectbox(
+    'How would you like to be contacted ?',
+    movies_list
+)
+
+if st.button('Recommend'):
+    names,posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
